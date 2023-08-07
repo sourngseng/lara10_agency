0b6df9fc85a9de95659baa48b7242f23a7b13ea1
SENG Sourng <120125764+sourngseng@users.noreply.github.com>
2023-08-07 20:54:10 +0700

    Update Menu

--- resources/views/layouts/partials/admin-left-sidebar.blade.php
+++ resources/views/layouts/partials/admin-left-sidebar.blade.php
@@ -15,32 +15,24 @@
   <div class="menu-block customscroll">
     <div class="sidebar-menu">
       <ul id="accordion-menu">
-        <li class="dropdown">
-          <a href="javascript:;" class="dropdown-toggle">
+        <li>
+          <a href="javascript:;" class="dropdown-toggle no-arrow">
             <span class="micon bi bi-house"></span
             ><span class="mtext">Home</span>
           </a>
-          <ul class="submenu">
-            <li><a href="index.html">Dashboard style 1</a></li>
-            <li><a href="index2.html">Dashboard style 2</a></li>
-            <li><a href="index3.html">Dashboard style 3</a></li>
-          </ul>
         </li>
+
         <li class="dropdown">
           <a href="javascript:;" class="dropdown-toggle">
             <span class="micon bi bi-textarea-resize"></span
-            ><span class="mtext">Forms</span>
+            ><span class="mtext">Manage Services</span>
           </a>
           <ul class="submenu">
-            <li><a href="form-basic.html">Form Basic</a></li>
+            <li><a href="form-basic.html">Create Service</a></li>
             <li>
-              <a href="advanced-components.html">Advanced Components</a>
+              <a href="advanced-components.html">All Services</a>
             </li>
-            <li><a href="form-wizard.html">Form Wizard</a></li>
-            <li><a href="html5-editor.html">HTML5 Editor</a></li>
-            <li><a href="form-pickers.html">Form Pickers</a></li>
-            <li><a href="image-cropper.html">Image Cropper</a></li>
-            <li><a href="image-dropzone.html">Image Dropzone</a></li>
+            <li><a href="form-wizard.html">Deleted Services</a></li>
           </ul>
         </li>
         <li class="dropdown">
